077f22153ac88b98f14c7b0600743596b3c907de
Vince Aggrippino <vinceaggrippino@gmail.com>
2019-12-03 15:41:06 +0800

    Switch to WEBM image format

--- js/getImageProperties.js
+++
@@ -1,6 +0,0 @@
-export function getImageProperties(image) {
-  return {
-    src: image.getAttribute(`src`),
-    alt: image.getAttribute(`alt`),
-  }
-}
--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-import { getImageProperties } from './getImageProperties.js';
-
 window.addEventListener(`load`, () => {
   const selector_buttons = document.querySelectorAll(`.product--images--selectors button`);
 
   selector_buttons.forEach((button) => {
     button.addEventListener(`click`, (e) => {
+      const display_picture = document.querySelector(`.product--images--display picture`);
+      const display_source1 = document.querySelector(`.product--images--display source:first-of-type`);
+      const display_source2 = document.querySelector(`.product--images--display source:nth-of-type(2)`);
       const display_image = document.querySelector(`.product--images--display img`);
       const display_link = document.querySelector(`.product--images--display a`);
-      const target_image = (e.target.tagName === `IMG`) ? e.target : e.target.firstChild;
-      const { src, alt } = getImageProperties( target_image );
 
-      display_link.setAttribute(`href`, src);
-      display_image.setAttribute(`src`, src);
-      display_image.setAttribute(`data-zoom`, src);
-      display_image.setAttribute(`alt`, alt);
+      const target_picture = (e.target.tagName === `IMG`) ? e.target.parentNode : e.target.firstChild;
+      const target_source1 = target_picture.querySelector(`source:first-child`);
+      const target_source2 = target_picture.querySelector(`source:nth-child(2)`);
+      const target_image = target_picture.querySelector(`img`);
+
+      display_source1.srcset = target_source1.dataset.fullsize;
+      display_source2.srcset = target_source2.dataset.fullsize;
+      display_image.src = target_image.src;
+      display_image.alt = target_image.alt;
+      display_link.href = target_image.src;
     });
   });
 
